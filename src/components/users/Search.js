215324a3c67fc5e361085710c89ffcf56628b927
Wes Jonke <wesjonke@gmail.com>
2019-10-20 15:08:49 -0400

    converted search component to hooks and functional components

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,46 +1,32 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-export class Search extends Component {
-  state={
-    text: ''
-  }
+export const Search =( { searchUsers, showClear, clearUsers,setAlert } ) => {
+  const [text, setText] = useState('')
   
-  static propTypes ={
-    searchUsers:PropTypes.func.isRequired,
-    clearUsers:PropTypes.func.isRequired,
-    showClear:PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  }
-
-  //this onChange allows us to type into the field and what we type becomes the state of the field
-  //[event.target.name] allows us to re-use this function with other form inputs by making the key dynamic!
-  onChange= (event) =>  this.setState({ [event.target.name] : event.target.value })
+  const onChange= (event) =>  setText(event.target.value )
   
-  
-  //this is where we add the logic for our helper function onSubmit passed into form
-  onSubmit= (event) => {
-
+  const onSubmit= (event) => {
     event.preventDefault();
 
-    if (this.state.text === ''){
-      this.props.setAlert('Please enter Something', 'light');
+    if (text === ''){
+      setAlert('Please enter Something', 'light');
     }else{
-      this.props.searchUsers(this.state.text)
-      this.setState({ text: '' });
+      searchUsers(text)
+      setText('');
     }
   }
 
 
-  render() {
+  
     return (
       <div>
 
-        <form className='form' onSubmit={this.onSubmit}> {/*onSubmit gives us functionality when the form is submitted */}
+        <form className='form' onSubmit={onSubmit}> {/*onSubmit gives us functionality when the form is submitted */}
 
           <input  type="text" 
-                  value={this.state.text} 
-                  onChange={this.onChange} 
+                  value={text} 
+                  onChange={onChange} 
                   name='text' 
                   placeholder='Search Users...'/>
 
@@ -49,18 +35,24 @@ export class Search extends Component {
                   className="btn btn-dark btn-block"/>
         </form>
 
-        {this.props.showClear && (
+        {showClear && (
           <button 
             className="btn btn-light btn-block" 
-            onClick={this.props.clearUsers}> Clear Users
+            onClick={clearUsers}> Clear Users
           </button>
         )}
 
           {/* this function makes it so we only show the button when the statement is true, IE there are users displayed. we are using a short circuit in this function && */}
 
       </div>
+    
     )
-  }
 }
 
+Search.propTypes ={
+  searchUsers:PropTypes.func.isRequired,
+  clearUsers:PropTypes.func.isRequired,
+  showClear:PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+}
 export default Search
