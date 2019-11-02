e25043358f632dfac83bb97739da775e64f85cdb
Wes Jonke <wesjonke@gmail.com>
2019-11-02 10:11:07 -0400

    fixed typo in About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <>
       <h1> About this App </h1>
-      <p> This is app to search for Github Users using the following build type:<li>Functional Components</li><li>React Hooks</li> <li>Context API for State Management</li> </p>
+      <p> This is an app to search for Github Users using the following build type:<li>Functional Components</li><li>React Hooks</li> <li>Context API for State Management</li> </p>
       <p> Version: 2.0.0 </p>
       <Link to='/' className="btn btn-dark btn-sm my-1"> Back to Search </Link>
     </>
