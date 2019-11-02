808fee1b2b3b2fb93a83e4af4b0a7097739d3523
Wes Jonke <wesjonke@gmail.com>
2019-11-02 10:34:53 -0400

    changed routes in app to reflect home component not needing props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {Switch, Route}  from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home'
 import User from './components/users/User';
-
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 
@@ -21,13 +20,7 @@ const App = () => {
 
         <div className="container">
           <Switch>
-            <Route exact path='/' render={props => (
-              <>
-                <Home/> 
-              </>
-              )} 
-            />
-          
+            <Route exact path='/' component={Home} /> 
             <Route exact path='/about' component={About} />
             <Route exact path='/user/:login' component={User} />
           </Switch>
